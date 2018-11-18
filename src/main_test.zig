6e4d75b0988e906a8103735d0b90b50152ff2726
gernest <geofreyernest@live.com>
2018-11-18 08:19:33 +0300

    add print tests

--- src/main_test.zig
+++ src/main_test.zig
@@ -2,7 +2,7 @@ const semver = @import("main.zig");
 const t = @import("./util/index.zig");
 const std = @import("std");
 const mem = std.mem;
-const warn = stddebug.warn;
+const warn = std.debug.warn;
 
 const testCase = struct {
     v: []const u8,
@@ -350,5 +350,48 @@ test "compare" {
         if (ts.cmp != cmp) {
             try t.terrorf("{} [{} ,{}]expected {} got {}", i, ts.x, ts.y, ts.cmp, cmp);
         }
+    }
+}
+
+const printTest = struct {
+    src: []const u8,
+    expect: []const u8,
+};
+
+const print_tests = []printTest{
+    printTest{ .src = "v1.0.0-alpha", .expect = "v1.0.0-alpha" },
+    printTest{ .src = "v1.0.0-alpha.1", .expect = "v1.0.0-alpha.1" },
+    printTest{ .src = "v1.0.0-alpha.beta", .expect = "v1.0.0-alpha.beta" },
+    printTest{ .src = "v1.0.0-beta", .expect = "v1.0.0-beta" },
+    printTest{ .src = "v1.0.0-beta.2", .expect = "v1.0.0-beta.2" },
+    printTest{ .src = "v1.0.0-beta.11", .expect = "v1.0.0-beta.11" },
+    printTest{ .src = "v1.0.0-rc.1", .expect = "v1.0.0-rc.1" },
+    printTest{ .src = "v1", .expect = "v1.0.0" },
+    printTest{ .src = "v1.0", .expect = "v1.0.0" },
+    printTest{ .src = "v1.0.0", .expect = "v1.0.0" },
+    printTest{ .src = "v1.2", .expect = "v1.2.0" },
+    printTest{ .src = "v1.2.0", .expect = "v1.2.0" },
+    printTest{ .src = "v1.2.3-456", .expect = "v1.2.3-456" },
+    printTest{ .src = "v1.2.3-456.789", .expect = "v1.2.3-456.789" },
+    printTest{ .src = "v1.2.3-456-789", .expect = "v1.2.3-456-789" },
+    printTest{ .src = "v1.2.3-456a", .expect = "v1.2.3-456a" },
+    printTest{ .src = "v1.2.3-pre", .expect = "v1.2.3-pre" },
+    printTest{ .src = "v1.2.3-pre+meta", .expect = "v1.2.3-pre+meta" },
+    printTest{ .src = "v1.2.3-pre.1", .expect = "v1.2.3-pre.1" },
+    printTest{ .src = "v1.2.3-zzz", .expect = "v1.2.3-zzz" },
+    printTest{ .src = "v1.2.3", .expect = "v1.2.3" },
+    printTest{ .src = "v1.2.3+meta", .expect = "v1.2.3+meta" },
+};
+
+test "print" {
+    var buf = try std.Buffer.init(std.debug.global_allocator, "");
+    defer buf.deinit();
+    for (print_tests) |ts| {
+        const v = try semver.parse(ts.src);
+        try v.printBuffer(&buf);
+        if (!mem.eql(u8, buf.toSlice(), ts.expect)) {
+            _ = t.terrorf("expected {} got {} {}", ts.expect, buf.toSlice(), v);
+        }
+        buf.shrink(0);
     }
 }
