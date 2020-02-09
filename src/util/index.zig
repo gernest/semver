0a0fcc1a4af7d269b5dfb553fbda5c2a9525c4c5
joachimschmidt557 <joachim.schmidt557@outlook.com>
2020-02-09 19:07:43 +0100

    more fixes

--- src/util/index.zig
+++ src/util/index.zig
@@ -1,11 +1,11 @@
 const debug = @import("std").debug;
 
 pub fn terror(comptime fmt: []const u8) !void {
-    debug.warn("{}\n", fmt);
+    debug.warn("{}\n", .{fmt});
     return error.TestFailed;
 }
 
-pub fn terrorf(comptime fmt: []const u8, args: ...) !void {
+pub fn terrorf(comptime fmt: []const u8, args: var) !void {
     debug.warn(fmt ++ "\n", args);
     return error.TestFailed;
 }
